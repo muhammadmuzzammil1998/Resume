bf38a883fc28e9cf68bf9856a6be612f21796c5a
Muhammad Muzzammil <muhammadmuzzammil.cs@gmail.com>
2019-12-25 02:18:51 +0530

    Update logic

--- resume.go
+++ resume.go
@@ -29,44 +29,43 @@ func check(i int, section []string) string {
 	return ""
 }
 
+func fatal(err ...error) {
+	for _, e := range err {
+		if e != nil {
+			log.Fatal(e)
+		}
+	}
+}
+
 func main() {
 	var (
 		header, errh   = readLines("parts/header.tex")
 		section1, err1 = readLines("parts/section1.tex")
 		section2, err2 = readLines("parts/section2.tex")
 	)
-	if errh != nil {
-		log.Fatal(errh)
-		return
+
+	fatal(errh, err1, err2)
+
+	for _, line := range header {
+		fmt.Println(line)
 	}
-	if err1 == nil {
-		if err2 != nil {
-			log.Fatal(err2)
-			return
+	for i := range section1 {
+		if i == 45 {
+			break
 		}
-		for _, line := range header {
-			fmt.Println(line)
-		}
-		for i := range section1 {
-			if i == 45 {
-				break
-			}
-			var (
-				first  = check(i, section1)
-				second = check(i, section2)
-			)
-			fmt.Printf("        %s & %s \\\\\n", first, second)
-		}
-		fmt.Printf("    \\end{tabular}\\\\\n")
-		if len(section1) > 45 {
-			for i := range section1[44:] {
-				fmt.Printf("    %s\\\\\n", section1[i+44])
-			}
-		}
-		fmt.Printf("    \\gitlinkc{muhammadmuzzammil1998/Resume}{\\tiny{Generated on \\today}}\n")
-		fmt.Printf("\\end{document}\n")
-		return
+		var (
+			first  = check(i, section1)
+			second = check(i, section2)
+		)
+		fmt.Printf("        %s & %s \\\\\n", first, second)
 	}
-	log.Fatal(err1)
-	return
+	fmt.Printf("    & \\hfill \\itshape \\color{icon} \\tiny{continued on next page...}\n")
+	fmt.Printf("    \\end{tabular}\\\\\n")
+	if len(section1) > 45 {
+		for i := range section1[45:] {
+			fmt.Printf("    %s\\\\\n", section1[i+45])
+		}
+	}
+	fmt.Printf("    \\gitlinkc{muhammadmuzzammil1998/Resume}{\\tiny{Generated on \\today}}\n")
+	fmt.Printf("\\end{document}\n")
 }
